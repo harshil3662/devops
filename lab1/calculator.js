6cf61ac655fa53fcdb09737ae4dd03fd8c3b3e21
harshil3662 <vaghaniharshil11291@gmail.com>
2024-02-18 18:02:30 -0500

    working on hook 3

--- lab1/calculator.js
+++ lab1/calculator.js
@@ -4,9 +4,9 @@ function add(a, b) {
   return a + b;
 }
   
-// function subtract(a, b) {
-//   return a - b;
-// }
+function subtract(a, b) {
+  return a - b;
+}
 
 function multiplication(a, b){
   return a*b;
