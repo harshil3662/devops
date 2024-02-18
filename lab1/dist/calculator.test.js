690ba26954d4b732f330674998a6fc17b0ce1cd6
harshil3662 <vaghaniharshil11291@gmail.com>
2024-02-17 20:02:20 -0500

    creating an artifact after some changes

---
+++ lab1/dist/calculator.test.js
@@ -0,0 +1,16 @@
+const calculator = require('./calculator');
+
+describe('Calculator', () => {
+  
+  test('adds 1 + 2 to equal 3', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  });
+
+  test('subtracts 4 - 2 to equal 2', () => {
+    expect(calculator.subtract(4, 2)).toBe(2);
+  });
+
+  test('multiply 5 * 2 to equal 10', () => {
+    expect(calculator.multiplication(5, 2)).toBe(10);
+  });
+});
